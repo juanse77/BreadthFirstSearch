7a97d58865e47cfe937cf2481636a0998cda8d8c
Pablo Montesdeoca Pérez <pmontesd@gmail.com>
2021-04-03 03:08:58 +0100

    Small fixes

--- AStar/mainAnalysisOfAStar.m
+++ AStar/mainAnalysisOfAStar.m
@@ -1,10 +1,6 @@
 clear;
 clc;
 
-% Access to functions for Breadth First Search implementation of route planning  
-currpath = path;
-addpath('../AStar/');
-
 resultAStar = [];
 for n = 15:55
     map_name = sprintf("../Maps/map_%dx%d.mat", n, n);
@@ -14,7 +10,5 @@ for n = 15:55
     resultAStar = [resultAStar; n, count];
     fprintf("n=%d\tcount=%d\n", n, count);
 end
-
-path(currpath);
 
 plot(resultAStar(:, 1), resultAStar(:, 2))
